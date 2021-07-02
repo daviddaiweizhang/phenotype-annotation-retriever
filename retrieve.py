8ba7713c870f085b7194c5a26a61395a581c0f05
David Zhang <zdavid0123@gmail.com>
2021-07-02 14:32:28 -0400

    clean up pheno string

--- retrieve.py
+++ retrieve.py
@@ -17,14 +17,14 @@ def convert_coordinate(
 
 
 def get_pheno_annot(
-        chrom, pos, ref_allele, alt_allele, reference_genome,
+        chrom, pos, ref_allele, alt_allele, ref_genome,
         n_top_pheno=10, pval_threshold=0.05,
         gwas_name='UKB-TOPMed', gwas_build='hg38'):
 
-    if reference_genome != gwas_build:
+    if ref_genome != gwas_build:
         pos = convert_coordinate(
                 chrom, pos,
-                build_in=reference_genome,
+                build_in=ref_genome,
                 build_out=gwas_build)[1]
     snp = f'{chrom}:{pos}-{ref_allele}-{alt_allele}'
     url = f'https://pheweb.org/{gwas_name}/api/variant/{snp}'
@@ -33,33 +33,38 @@ def get_pheno_annot(
     if info is None:
         phenos_top_str = ''
     else:
+        # retrieve phenotype names
         phenos = [
-                pheno['phenostring'].replace(' ', '-')
+                pheno['phenostring']
                 for pheno in info['phenos']]
+        # retrieve phenotype pvalues
         pvals = [
                 pheno['pval']
                 for pheno in info['phenos']]
-
+        # sort by pvalues and filter by significance
         phenos_signi = [
                 pheno for pval, pheno
                 in sorted(zip(pvals, phenos))
                 if pval < pval_threshold]
+        # select the top phenotypes
         phenos_top = phenos_signi[:n_top_pheno]
-        phenos_top_str = '|'.join(phenos_top)
+        # convert to camel case, remove punctuations, concat
+        phenos_top_str = '|'.join([''.join(filter(
+                str.isalnum, pheno.title().replace(' ', '')))
+                for pheno in phenos_top])
     return phenos_top_str
 
 
-def get_pheno_annot_from_rsid(rsid, reference_genome):
+def get_pheno_annot_from_rsid(rsid, ref_genome):
     chrom, pos, ref_allele, alt_allele = rsid_to_coordinate(
             rsid=rsid,
-            reference_genome=reference_genome)
-    print(chrom, pos, ref_allele, alt_allele)
+            ref_genome=ref_genome)
     return get_pheno_annot(
         chrom=chrom,
         pos=pos,
         ref_allele=ref_allele,
         alt_allele=alt_allele,
-        reference_genome=reference_genome)
+        ref_genome=ref_genome)
 
 
 def main():
@@ -88,7 +93,7 @@ def main():
                 'Alternative allele of the SNP. '
                 'Example: C. '))
     parser.add_argument(
-            '--reference-genome',
+            '--ref-genome',
             help=(
                 'Build of the reference genome. '
                 'Example: hg19. '))
@@ -105,11 +110,11 @@ def main():
                 pos=args['pos'],
                 ref_allele=args['ref_allele'],
                 alt_allele=args['alt_allele'],
-                reference_genome=args['reference_genome'])
+                ref_genome=args['ref_genome'])
     else:
         pheno_annot = get_pheno_annot_from_rsid(
                 rsid=args['rsid'],
-                reference_genome='hg38')
+                ref_genome='hg38')
     print(pheno_annot)
 
 
